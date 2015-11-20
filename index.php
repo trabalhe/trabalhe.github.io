e4f1f4fdd1720078f937a3d5ff32e07398869e68
Rinaldo Melo <rinaldomelo72@gmail.com>
2015-11-20 19:26:32 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="bower_components/Materialize/bin/materialize.css" media="screen,projection" />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta charset="UTF-8">
+    <title>Trabalhe</title>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="js/main.js"></script>
+    
+</head>
+
+<body>
+   
+     <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Trabalhe</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="signup.php">Seja PRO</a></li>
+        <li><a href="signup.php">Cadastre-se</a></li>
+        <li><a href="login.php">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+    
+    
+    
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="bower_components/Materialize/bin/materialize.js"></script>
+    </body>
+    
+</body>
+
+</html>
---
+++ php_includes/db_conx.php
@@ -0,0 +1,8 @@
+<?php
+$db_conx = mysqli_connect("trabalhe.elipsdanceproject.com", "rinaldomelo72", "17913111414", "trabalhe");
+// Evaluate the connection
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+    exit();
+}
+?>
